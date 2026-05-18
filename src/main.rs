befa56095ca2b99fea6a3c569f21c1ccaa07c2e6
Georgiy Savchenko <gesav@live.com>
2026-05-18 16:59:53 +0100

    simple web search implemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 mod search;
 
-fn main() {
-    println!("Hello, Perplexed!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = reqwest::Client::new();
+
+    let searxng_base_url = "http://localhost:8888";
+    let query = "What is Perplexity";
+    let top_k = 5;
+
+    let search_results = search::search(&client, searxng_base_url, query, top_k).await?;
+
+    for r in &search_results {
+        println!("[{}] {} - {} (score: {})", r.engine, r.title, r.url, r.score);
+    }
+
+    Ok(())
 }
--- src/search.rs
+++ src/search.rs
@@ -12,3 +12,25 @@ pub struct SearchHit {
 struct SearchHits {
     results: Vec<SearchHit>,
 }
+
+
+pub async fn search(
+    client: &reqwest::Client,
+    base_url: &str,
+    user_query: &str,
+    result_cap: usize
+) -> anyhow::Result<Vec<SearchHit>> {
+
+    let url_endpoint = format!("{}/search", base_url);
+    let search_hits = client
+        .get(url_endpoint)
+        .query(&[("q", user_query), ("format", "json")])
+        .send().await?
+        .error_for_status()?
+        .json::<SearchHits>().await?;
+
+    let mut results = search_hits.results;
+    results.truncate(result_cap);
+    
+    Ok(results)
+}
